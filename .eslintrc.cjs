4c73e179cae0c0b881da36095745d8c54fcf6a34
OrvokkiK <orvokki.kettula@metropolia.fi>
2024-03-20 14:51:43 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'google',
+  'overrides': [],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'indent': ['warn', 2],
+    'new-cap': ['error', {capIsNewExceptions: ['Router']}],
+    'linebreak-style': ['error', 'windows'],
+  },
+};
