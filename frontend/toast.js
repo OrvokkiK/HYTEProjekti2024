b0fa396b5b9e7a9a014924efadae613f61e5fed7
ainoruo <aino.ruohola@metropolia.fi>
2024-04-04 14:36:21 +0300

    add changes

--- frontend/toast.js
+++ frontend/toast.js
@@ -1,15 +1,15 @@
+// eslint-disable-next-line require-jsdoc
 function showToast(message) {
+  const toastContainer = document.getElementById('toast-container');
+  const toast = document.createElement('div');
+  toast.className = 'toast';
+  toast.textContent = message;
 
-    const toastContainer = document.getElementById('toast-container');
-    const toast = document.createElement('div');
-    toast.className = 'toast';
-    toast.textContent = message;
-   
-    toastContainer.appendChild(toast);
-  
-    setTimeout(() => {
-      toastContainer.removeChild(toast);
-    }, 6000);
-  }
+  toastContainer.appendChild(toast);
 
-  export {showToast};
+  setTimeout(() => {
+    toastContainer.removeChild(toast);
+  }, 6000);
+}
+
+export {showToast};
