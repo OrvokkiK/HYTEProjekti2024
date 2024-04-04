f295988bb0d04da071ef1c4de2fc124ecd37c778
ainoruo <aino.ruohola@metropolia.fi>
2024-04-04 12:18:37 +0300

    add changes to graph

--- frontend/home.js
+++ frontend/home.js
@@ -4,19 +4,35 @@ let today = new Date();
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 
-let monthNames = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"];
+let monthNames = [
+  "Tammikuu",
+  "Helmikuu",
+  "Maaliskuu",
+  "Huhtikuu",
+  "Toukokuu",
+  "Kesäkuu",
+  "Heinäkuu",
+  "Elokuu",
+  "Syyskuu",
+  "Lokakuu",
+  "Marraskuu",
+  "Joulukuu",
+];
 
-document.getElementById("prevMonth").addEventListener("click", () => previous());
+document
+  .getElementById("prevMonth")
+  .addEventListener("click", () => previous());
 document.getElementById("nextMonth").addEventListener("click", () => next());
 
 function showCalendar(month, year) {
-  let firstDay = (new Date(year, month).getDay() + 6) % 7; 
+  let firstDay = (new Date(year, month).getDay() + 6) % 7; // Make Monday (0), Sunday (6)
   let daysInMonth = 32 - new Date(year, month, 32).getDate();
 
   let tbl = document.getElementById("calendar-body");
   tbl.innerHTML = "";
 
-  document.getElementById("monthAndYear").innerText = monthNames[month] + " " + year;
+  document.getElementById("monthAndYear").innerText =
+    monthNames[month] + " " + year;
 
   let date = 1;
   for (let i = 0; i < 6; i++) {
@@ -33,7 +49,11 @@ function showCalendar(month, year) {
       } else {
         let cellText = document.createTextNode(date);
         cell.appendChild(cellText);
-        if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+        if (
+          date === today.getDate() &&
+          year === today.getFullYear() &&
+          month === today.getMonth()
+        ) {
           cell.classList.add("current-date"); // Highlight the current date
         }
         row.appendChild(cell);
@@ -45,21 +65,19 @@ function showCalendar(month, year) {
   }
 }
 
-
 function previous() {
-  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-  currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
   showCalendar(currentMonth, currentYear);
 }
 
 function next() {
-  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
   currentMonth = (currentMonth + 1) % 12;
   showCalendar(currentMonth, currentYear);
 }
 
 showCalendar(currentMonth, currentYear);
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".menu-toggle");
@@ -72,27 +90,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Oirearviokyselyn toiminnallisuudet
 const surveyModal = document.getElementById("survey-modal");
-const formMental = document.getElementById('survey-form-mental');
-const formPhysical = document.getElementById('survey-form-physical');
+const formMental = document.getElementById("survey-form-mental");
+const formPhysical = document.getElementById("survey-form-physical");
 const button = document.getElementById("openArvioKyselyModal");
 const closeButton = document.querySelector(".close-button");
 
-button.onclick = function() {
+button.onclick = function () {
   surveyModal.style.display = "block";
-  formMental.style.display = "block"; 
+  formMental.style.display = "block";
   formPhysical.style.display = "none";
 };
 
-document.querySelector('.next-button').addEventListener('click', function() {
-  formMental.style.display = 'none';
-  formPhysical.style.display = 'block';
+document.querySelector(".next-button").addEventListener("click", function () {
+  formMental.style.display = "none";
+  formPhysical.style.display = "block";
 });
 
-document.querySelector('.prev-button').addEventListener('click', function() {
-  formPhysical.style.display = 'none';
-  formMental.style.display = 'block';
+document.querySelector(".prev-button").addEventListener("click", function () {
+  formPhysical.style.display = "none";
+  formMental.style.display = "block";
 });
-
 
 closeButton.onclick = () => (surveyModal.style.display = "none");
 
@@ -110,9 +127,9 @@ function submitForm1() {
 
 const button2 = document.getElementById("openElamantapaKyselyModal");
 
-button2.onclick = function() {
+button2.onclick = function () {
   sleepModal.style.display = "block";
-}
+};
 
 const form2 = document.getElementById("sleep-form");
 form2.addEventListener("submit", submitForm2);
@@ -121,470 +138,279 @@ const sleepModal = document.getElementById("sleep-modal");
 const closeButton2 = document.querySelector(".close-button2");
 
 closeButton2.onclick = () => {
-    sleepModal.style.display = "none";
+  sleepModal.style.display = "none";
 };
 
 function submitForm2() {
   console.log("elämantapa modal toimii");
 }
 
-// graph code
-
-am5.ready(function() {
-
+// Graph code
+am5.ready(function () {
   // Create root element
-  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
   var root = am5.Root.new("graph");
-  
-  
+
   // Set themes
-  // https://www.amcharts.com/docs/v5/concepts/themes/
-  root.setThemes([
-    am5themes_Animated.new(root)
-  ]);
-  
-  root.dateFormatter.setAll({
-    dateFormat: "yyyy",
-    dateFields: ["valueX"]
-  });
-  
-  var data = [ {
-    "date": "2012-10-12",
-    "value": 31
-  }, {
-    "date": "2012-10-13",
-    "value": 30
-  }, {
-    "date": "2012-10-14",
-    "value": 34
-  }, {
-    "date": "2012-10-15",
-    "value": 38
-  }, {
-    "date": "2012-10-16",
-    "value": 37
-  }, {
-    "date": "2012-10-17",
-    "value": 44
-  }, {
-    "date": "2012-10-18",
-    "value": 49
-  }, {
-    "date": "2012-10-19",
-    "value": 53
-  }, {
-    "date": "2012-10-20",
-    "value": 57
-  }, {
-    "date": "2012-10-21",
-    "value": 60
-  }, {
-    "date": "2012-10-22",
-    "value": 61
-  }, {
-    "date": "2012-10-23",
-    "value": 69
-  }, {
-    "date": "2012-10-24",
-    "value": 67
-  }, {
-    "date": "2012-10-25",
-    "value": 72
-  }, {
-    "date": "2012-10-26",
-    "value": 77
-  }, {
-    "date": "2012-10-27",
-    "value": 75
-  }, {
-    "date": "2012-10-28",
-    "value": 70
-  }, {
-    "date": "2012-10-29",
-    "value": 72
-  }, {
-    "date": "2012-10-30",
-    "value": 70
-  }, {
-    "date": "2012-10-31",
-    "value": 72
-  }, {
-    "date": "2012-11-01",
-    "value": 73
-  }, {
-    "date": "2012-11-02",
-    "value": 67
-  }, {
-    "date": "2012-11-03",
-    "value": 68
-  }, {
-    "date": "2012-11-04",
-    "value": 65
-  }, {
-    "date": "2012-11-05",
-    "value": 71
-  }, {
-    "date": "2012-11-06",
-    "value": 75
-  }, {
-    "date": "2012-11-07",
-    "value": 74
-  }, {
-    "date": "2012-11-08",
-    "value": 71
-  }, {
-    "date": "2012-11-09",
-    "value": 76
-  }, {
-    "date": "2012-11-10",
-    "value": 77
-  }, {
-    "date": "2012-11-11",
-    "value": 81
-  }, {
-    "date": "2012-11-12",
-    "value": 83
-  }, {
-    "date": "2012-11-13",
-    "value": 80
-  }, {
-    "date": "2012-11-14",
-    "value": 81
-  }, {
-    "date": "2012-11-15",
-    "value": 87
-  }, {
-    "date": "2012-11-16",
-    "value": 82
-  }, {
-    "date": "2012-11-17",
-    "value": 86
-  }, {
-    "date": "2012-11-18",
-    "value": 80
-  }, {
-    "date": "2012-11-19",
-    "value": 87
-  }, {
-    "date": "2012-11-20",
-    "value": 83
-  }, {
-    "date": "2012-11-21",
-    "value": 85
-  }, {
-    "date": "2012-11-22",
-    "value": 84
-  }, {
-    "date": "2012-11-23",
-    "value": 82
-  }, {
-    "date": "2012-11-24",
-    "value": 73
-  }, {
-    "date": "2012-11-25",
-    "value": 71
-  }, {
-    "date": "2012-11-26",
-    "value": 75
-  }, {
-    "date": "2012-11-27",
-    "value": 79
-  }, {
-    "date": "2012-11-28",
-    "value": 70
-  }, {
-    "date": "2012-11-29",
-    "value": 73
-  }, {
-    "date": "2012-11-30",
-    "value": 61
-  }, {
-    "date": "2012-12-01",
-    "value": 62
-  }, {
-    "date": "2012-12-02",
-    "value": 66
-  }, {
-    "date": "2012-12-03",
-    "value": 65
-  }, {
-    "date": "2012-12-04",
-    "value": 73
-  }, {
-    "date": "2012-12-05",
-    "value": 79
-  }, {
-    "date": "2012-12-06",
-    "value": 78
-  }, {
-    "date": "2012-12-07",
-    "value": 78
-  }, {
-    "date": "2012-12-08",
-    "value": 78
-  }, {
-    "date": "2012-12-09",
-    "value": 74
-  }, {
-    "date": "2012-12-10",
-    "value": 73
-  }, {
-    "date": "2012-12-11",
-    "value": 75
-  }, {
-    "date": "2012-12-12",
-    "value": 70
-  }, {
-    "date": "2012-12-13",
-    "value": 77
-  }, {
-    "date": "2012-12-14",
-    "value": 67
-  }, {
-    "date": "2012-12-15",
-    "value": 62
-  }, {
-    "date": "2012-12-16",
-    "value": 64
-  }, {
-    "date": "2012-12-17",
-    "value": 61
-  }, {
-    "date": "2012-12-18",
-    "value": 59
-  }, {
-    "date": "2012-12-19",
-    "value": 53
-  }, {
-    "date": "2012-12-20",
-    "value": 54
-  }, {
-    "date": "2012-12-21",
-    "value": 56
-  }, {
-    "date": "2012-12-22",
-    "value": 59
-  }, {
-    "date": "2012-12-23",
-    "value": 58
-  }, {
-    "date": "2012-12-24",
-    "value": 55
-  }, {
-    "date": "2012-12-25",
-    "value": 52
-  }, {
-    "date": "2012-12-26",
-    "value": 54
-  }, {
-    "date": "2012-12-27",
-    "value": 50
-  }, {
-    "date": "2012-12-28",
-    "value": 50
-  }, {
-    "date": "2012-12-29",
-    "value": 51
-  }, {
-    "date": "2012-12-30",
-    "value": 52
-  }, {
-    "date": "2012-12-31",
-    "value": 58
-  }, {
-    "date": "2013-01-01",
-    "value": 60
-  }, {
-    "date": "2013-01-02",
-    "value": 67
-  }, {
-    "date": "2013-01-03",
-    "value": 64
-  }, {
-    "date": "2013-01-04",
-    "value": 66
-  }, {
-    "date": "2013-01-05",
-    "value": 60
-  }, {
-    "date": "2013-01-06",
-    "value": 63
-  }, {
-    "date": "2013-01-07",
-    "value": 61
-  }, {
-    "date": "2013-01-08",
-    "value": 60
-  }, {
-    "date": "2013-01-09",
-    "value": 65
-  }, {
-    "date": "2013-01-10",
-    "value": 75
-  }, {
-    "date": "2013-01-11",
-    "value": 77
-  }, {
-    "date": "2013-01-12",
-    "value": 78
-  }, {
-    "date": "2013-01-13",
-    "value": 70
-  }, {
-    "date": "2013-01-14",
-    "value": 70
-  }, {
-    "date": "2013-01-15",
-    "value": 73
-  }, {
-    "date": "2013-01-16",
-    "value": 71
-  }, {
-    "date": "2013-01-17",
-    "value": 74
-  }, {
-    "date": "2013-01-18",
-    "value": 78
-  }, {
-    "date": "2013-01-19",
-    "value": 85
-  }, {
-    "date": "2013-01-20",
-    "value": 82
-  }, {
-    "date": "2013-01-21",
-    "value": 83
-  }, {
-    "date": "2013-01-22",
-    "value": 88
-  }, {
-    "date": "2013-01-23",
-    "value": 85
-  }, {
-    "date": "2013-01-24",
-    "value": 85
-  }, {
-    "date": "2013-01-25",
-    "value": 80
-  }, {
-    "date": "2013-01-26",
-    "value": 87
-  }, {
-    "date": "2013-01-27",
-    "value": 84
-  }, {
-    "date": "2013-01-28",
-    "value": 83
-  }, {
-    "date": "2013-01-29",
-    "value": 84
-  }, {
-    "date": "2013-01-30",
-    "value": 81
-  }];
-  
-  
+  root.setThemes([am5themes_Animated.new(root)]);
+
   // Create chart
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/
-  var chart = root.container.children.push(am5xy.XYChart.new(root, {
-    focusable: true,
-    panX: true,
-    panY: true,
-    wheelX: "panX",
-    wheelY: "zoomX",
-    pinchZoomX:true,
-    paddingLeft: 0
-  }));
-  
-  var easing = am5.ease.linear;
-  
-  
-  // Create axes
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-  var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
-    maxDeviation: 0.1,
-    groupData: false,
-    baseInterval: {
-      timeUnit: "day",
-      count: 1
+  var chart = root.container.children.push(
+    am5xy.XYChart.new(root, {
+      panX: false,
+      panY: false,
+      wheelX: "panX",
+      wheelY: "zoomX",
+      paddingLeft: 0,
+      paddingRight: 0,
+      layout: root.verticalLayout,
+    })
+  );
+
+  chart.children.unshift(
+    am5.Label.new(root, {
+      text: "Otsikko",
+      fontSize: 25,
+      fontWeight: "500",
+      textAlign: "center",
+      x: am5.percent(50),
+      centerX: am5.percent(50),
+      paddingTop: 0,
+      paddingBottom: 20,
+    })
+  );
+
+  // chart.children.unshift(
+  //   am5.Label.new(root, {
+  //     text: "Stressitaso",
+  //     fontSize: 15,
+  //     fontWeight: "300",
+  //     textAlign: "right",
+  //     x: am5.percent(50),
+  //     centerX: am5.percent(50),
+  //     paddingTop: 0,
+  //     paddingBottom: 20,
+  //   })
+  // );
+
+  var colors = chart.get("colors");
+
+  var data = [
+    {
+      date: "2012-07-27",
+      value: 1,
+      arvio: "Matala stressitaso",
+      hrv: 60,
     },
-    renderer: am5xy.AxisRendererX.new(root, {
-      minorGridEnabled: true,
-      minGridDistance: 70
-    }),
-    tooltip: am5.Tooltip.new(root, {})
+    {
+      date: "2012-07-28",
+      value: 1,
+      arvio: "Matala stressitaso",
+      hrv: 60,
+    },
+    {
+      date: "2012-07-29",
+      value: 3,
+      arvio: "Korkea stressitaso",
+      hrv: 40,
+    },
+    {
+      date: "2012-07-30",
+      value: 3,
+      arvio: "Korkea stressitaso",
+      hrv: 55,
+    },
+    {
+      date: "2012-07-31",
+      value: 3,
+      arvio: "Korkea stressitaso",
+      hrv: 35,
+    },
+    {
+      date: "2012-08-01",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 60,
+    },
+    {
+      date: "2012-08-02",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 45,
+    },
+    {
+      date: "2012-08-03",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 60,
+    },
+    {
+      date: "2012-08-04",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 70,
+    },
+    {
+      date: "2012-08-05",
+      value: 1,
+      arvio: "Matala stressitaso",
+      hrv: 60,
+    },
+    {
+      date: "2012-08-06",
+      value: 1,
+      arvio: "Matala stressitaso",
+      hrv: 55,
+    },
+    {
+      date: "2012-08-07",
+      value: 1,
+      arvio: "Matala stressitaso",
+      hrv: 50,
+    },
+    {
+      date: "2012-08-08",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 60,
+    },
+    {
+      date: "2012-08-09",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 60,
+    },
+    {
+      date: "2012-08-10",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 35,
+    },
+    {
+      date: "2012-08-11",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 50,
+    },
+    {
+      date: "2012-08-12",
+      value: 3,
+      arvio: "Korkea stressitaso",
+      hrv: 40,
+    },
+    {
+      date: "2012-08-13",
+      value: 1,
+      arvio: "Matala stressitaso",
+      hrv: 66,
+    },
+    {
+      date: "2012-08-14",
+      value: 2,
+      arvio: "Kohtalainen stressitaso",
+      hrv: 55,
+    },
+  ];
+
+  // Create axes
+  var xRenderer = am5xy.AxisRendererX.new(root, {
+    minGridDistance: 85,
+    minorGridEnabled: true,
+  });
+
+  var xAxis = chart.xAxes.push(
+    am5xy.CategoryAxis.new(root, {
+      categoryField: "date",
+      renderer: xRenderer,
+    })
+  );
+
+  xRenderer.grid.template.setAll({
+    location: 1,
+  });
+
+  xRenderer.labels.template.setAll({
+    paddingTop: 20,
+  });
+
+  xAxis.data.setAll(data);
+
+  var yAxis = chart.yAxes.push(
+    am5xy.ValueAxis.new(root, {
+      min: 1,
+      max: 3,
+      maxPrecision: 0,
+      renderer: am5xy.AxisRendererY.new(root, {
+        strokeOpacity: 0.1,
+      }),
+    })
+  );
+
+  var hrvAxisRenderer = am5xy.AxisRendererY.new(root, {
+    opposite: true,
+    inside: false, // Aseta false, jotta arvot näkyvät akselin ulkopuolella
+    maxLabelPosition: 0.98 // Aseta arvo alle 1 estääksesi labelien menemästä kaavion reunan yli
+  });
+
+  var hrvAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+    renderer: hrvAxisRenderer,
+    min: 0, // Säädä nämä HRV-datan mukaan
+    max: 100, // Säädä nämä HRV-datan mukaan
   }));
   
-  var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-    maxDeviation: 0.2,
-    renderer: am5xy.AxisRendererY.new(root, {})
-  }));
-  
-  
+  // Lisää padding labelien oikealle puolelle
+  hrvAxisRenderer.labels.template.setAll({
+    paddingRight: 25, // Säädä tämä arvo haluamaksesi paddingiksi
+  });
+
   // Add series
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-  var series = chart.series.push(am5xy.LineSeries.new(root, {
-    minBulletDistance: 10,
-    connect: false,
-    xAxis: xAxis,
-    yAxis: yAxis,
-    valueYField: "value",
-    valueXField: "date",
-    tooltip: am5.Tooltip.new(root, {
-      pointerOrientation: "horizontal",
-      labelText: "{valueY}"
+  var series = chart.series.push(
+    am5xy.ColumnSeries.new(root, {
+      xAxis: xAxis,
+      yAxis: yAxis,
+      valueYField: "value",
+      categoryXField: "date",
     })
-  }));
-  
-  series.fills.template.setAll({
-    fillOpacity: 0.2,
-    visible: true
+  );
+
+  series.columns.template.setAll({
+    tooltipText: "{categoryX}: {valueY}",
+    tooltipY: 0,
+    strokeOpacity: 0,
+    cornerRadiusTL: 6,
+    cornerRadiusTR: 6,
   });
-  
-  series.strokes.template.setAll({
-    strokeWidth: 2
+
+  series.columns.template.adapters.add("fill", function (fill, target) {
+    return chart
+      .get("colors")
+      .getIndex(series.dataItems.indexOf(target.dataItem));
   });
-  
-  
-  // Set up data processor to parse string dates
-  // https://www.amcharts.com/docs/v5/concepts/data/#Pre_processing_data
-  series.data.processor = am5.DataProcessor.new(root, {
-    dateFormat: "yyyy-MM-dd",
-    dateFields: ["date"]
-  });
-  
-  series.data.setAll(data);
-  
-  series.bullets.push(function() {
-    var circle = am5.Circle.new(root, {
-      radius: 4,
-      fill: root.interfaceColors.get("background"),
-      stroke: series.get("fill"),
-      strokeWidth: 2
+
+  var hrvSeries = chart.series.push(
+    am5xy.LineSeries.new(root, {
+      xAxis: xAxis,
+      yAxis: hrvAxis,
+      valueYField: "hrv",
+      categoryXField: "date",
+      stroke: root.interfaceColors.get("alternativeBackground"),
+      strokeWidth: 3,
     })
-  
+  );
+
+  hrvSeries.bullets.push(function () {
     return am5.Bullet.new(root, {
-      sprite: circle
-    })
+      sprite: am5.Circle.new(root, {
+        radius: 4,
+        fill: hrvSeries.get("stroke"),
+        tooltipText: "HRV: {hrv} ms",
+      }),
+    });
   });
-  
-  
-  // Add cursor
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-  var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
-    xAxis: xAxis,
-    behavior: "none"
-  }));
-  cursor.lineY.set("visible", false);
-  
-  // add scrollbar
-  chart.set("scrollbarX", am5.Scrollbar.new(root, {
-    orientation: "horizontal"
-  }));
-  
-  
-  // Make stuff animate on load
-  // https://www.amcharts.com/docs/v5/concepts/animations/
+
+  series.data.setAll(data);
+  hrvSeries.data.setAll(data);
+
+  series.appear();
+  hrvSeries.appear();
   chart.appear(1000, 100);
-  
-  }); // end am5.ready()
+});
