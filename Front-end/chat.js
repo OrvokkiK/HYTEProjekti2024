fcab44893997e52feff75e7ac1fa4667ed3cf118
ainoruo <aino.ruohola@metropolia.fi>
2024-04-25 10:28:46 +0300

    add changes to message page

--- Front-end/chat.js
+++ Front-end/chat.js
@@ -1,3 +1,7 @@
+import './style.css';
+import {fetchData} from './fetch.js';
+import {showToast} from './toast.js';
+
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const menu = document.querySelector('.menu');
@@ -6,6 +10,54 @@ document.addEventListener('DOMContentLoaded', function () {
       menu.classList.toggle('show');
     });
   });
+
+// chat
+document.addEventListener('DOMContentLoaded', function() {
+  const token = localStorage.getItem('token');
+  const id = localStorage.getItem('user_id');
+  const url = `http://localhost:3000/api/messages/converstation/${id}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  fetchData(url, options).then((data) => {
+    const chatContainer = document.getElementById('chat-container');
+    
+    // Tyhjennä chatContainer ennen uusien viestien lisäämistä
+    chatContainer.innerHTML = '';
+
+    data.forEach(message => {
+      const messageElement = document.createElement('div');
+      messageElement.classList.add('message');
+      // Lisää luokka määrittämään, kumpi lähetti viestin
+      if (message.sender_id === id) {
+        // Jos käyttäjä on lähettäjä, viestit oikealle
+        messageElement.classList.add('user-message');
+      } else {
+        // Jos ammattilainen on lähettäjä, viestit vasemmalle
+        messageElement.classList.add('professional-message');
+      }
+      
+      // Lisää viestin sisältö ja lähetysaika
+      messageElement.innerHTML = `
+        <div class="message-meta">
+          <span class="message-date">${new Date(message.message_sent_at).toLocaleDateString()}</span>
+        </div>
+        <div class="message-content">
+          ${message.message_content}
+        </div>
+      `;
+      
+      // Lisää viesti chatContaineriin
+      chatContainer.appendChild(messageElement);
+    });
+  });
+});
+
 
   // logout
 document.addEventListener('DOMContentLoaded', function () {
