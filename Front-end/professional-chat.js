62fe24d54cb35b9791353690b5e6e2c5dc8d296b
ainoruo <aino.ruohola@metropolia.fi>
2024-05-01 22:05:56 +0300

    add changes to messages

--- Front-end/professional-chat.js
+++ Front-end/professional-chat.js
@@ -1,105 +1,115 @@
 import './style.css';
-import { fetchData } from './fetch.js';
-import { showToast } from './toast.js';
+import {fetchData} from './fetch.js';
+import {showToast} from './toast.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
-  
-    menuToggle.addEventListener('click', function () {
-      menu.classList.toggle('show');
-    });
-  
-    // Haetaan käyttäjät ja näytetään lista
-    fetchUsers();
-  
-    const logoutLink = document.querySelector('.logout a');
-    logoutLink.addEventListener('click', function (event) {
-      event.preventDefault();
-      localStorage.removeItem('analysisModalShown');
-      localStorage.removeItem('user_id');
-      localStorage.removeItem('token');
-      showToast('Kirjaudutaan ulos.');
-      setTimeout(() => {
-        window.location.href = 'index.html';
-      }, 2000);
-    });
+  const sendButton = document.getElementById('send-button');
+  const messageInput = document.getElementById('message-content');
+  const recipientInput = document.getElementById('recipient-id');
+  const messagesContainer = document.getElementById('messages-container');
+  const userList = document.getElementById('user-list');
+  const token = localStorage.getItem('token');
+  const userId = localStorage.getItem('user_id');
+  console.log('User ID on load:', userId);
+  let currentConversationId = null;
 
-    const sendButton = document.getElementById('send-button');
-    sendButton.addEventListener('click', sendMessage);
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+  menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('show');
   });
-  
+  sendButton.addEventListener('click', sendMessage);
+
+  fetchUsers();
 
   async function fetchUsers() {
-    const token = localStorage.getItem('token');
-    const userId = localStorage.getItem('user_id');
-    const usersUrl = 'http://localhost:3000/api/users';
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  
+    const usersUrl = 'http://localhost:3000/api/users/';
     try {
-      const users = await fetchData(usersUrl, options);
-      console.log(users); 
-      displayUsers(users, userId);
+      const users = await fetchData(usersUrl, {
+        headers: {Authorization: `Bearer ${token}`},
+      });
+      displayUsers(users);
     } catch (error) {
-      console.error('Virhe käyttäjien haussa:', error);
-      showToast('Virhe käyttäjien haussa.');
+      console.error('Error fetching users:', error);
     }
   }
-  
-  function displayUsers(users, currentUserId) {
-    const userList = document.getElementById('conversation-list');
-    userList.innerHTML = ''; // Tyhjennetään lista ennen päivitystä
-  
+
+  function displayUsers(users) {
+    userList.innerHTML = '';
     users.forEach((user) => {
-      if (user.user_id !== currentUserId) {
-        const userItem = document.createElement('li');
-        userItem.textContent = user.username;
-        userItem.dataset.userId = user.user_id;
-        userItem.addEventListener('click', selectRecipient);
-        userList.appendChild(userItem);
-      }
+      const userItem = document.createElement('li');
+      userItem.textContent = user.username;
+      userItem.dataset.userId = user.user_id;
+      userItem.addEventListener('click', selectRecipient);
+      userList.appendChild(userItem);
     });
   }
-  
-  function selectRecipient(event) {
-    const recipientId = event.target.dataset.userId;
-    const recipientName = event.target.textContent;
-    const recipientInput = document.getElementById('recipient-input'); // Korjattu ID
-    recipientInput.value = recipientName;
-    recipientInput.dataset.userId = recipientId;
+
+  async function selectRecipient(event) {
+    const selectedUserId = event.target.dataset.userId;
+    recipientInput.dataset.userId = selectedUserId;
+    fetchConversationsAndMessagesForUser(selectedUserId);
   }
-  
-  async function sendMessage() {
-    const token = localStorage.getItem('token');
-    const senderId = localStorage.getItem('user_id');
-    const recipientInput = document.querySelector('[data-input-type="recipient"]');
-    const recipientId = recipientInput.dataset.userId;
-    const messageInput = document.querySelector('[data-input-type="message"]');
-    const messageContent = messageInput.value;
-  
-    if (!recipientId) {
-      showToast('Valitse ensin vastaanottaja.');
-      return;
+
+  async function fetchConversationsAndMessagesForUser(selectedUserId) {
+    const apiUrl = `http://localhost:3000/api/messages/user/${selectedUserId}`;
+    try {
+      const conversations = await fetchData(apiUrl, {
+        headers: {Authorization: `Bearer ${token}`},
+      });
+      messagesContainer.innerHTML = ''; // Tyhjennä viestit
+      if (conversations.length > 0) {
+        currentConversationId = conversations[0].conversation_id; // Oleta, että valitaan ensimmäinen keskustelu
+        conversations.forEach((conversation) =>
+          fetchMessagesForConversation(conversation.conversation_id),
+        );
+      } else {
+        currentConversationId = null; // Ei keskusteluja, tyhjennä nykyinen ID
+      }
+    } catch (error) {
+      console.error('Error fetching conversations:', error);
+      messagesContainer.innerHTML = 'Ei viestejä.';
     }
+  }
+
+  function fetchMessagesForConversation(conversationId, userId) {
+    const apiUrl = `http://localhost:3000/api/messages/conversation/${conversationId}`;
+    fetchData(apiUrl, {headers: {Authorization: `Bearer ${token}`}})
+      .then((messages) => {
+        displayMessages(messages, userId); // Varmista, että userId on tässä oikein
+      })
+      .catch((error) => {
+        console.error('Error fetching messages:', error);
+      });
+  }
+
+function displayMessages(messages, userId) {
+  messagesContainer.innerHTML = ''; // Tyhjennä viestit ennen uusien näyttämistä
+  messages.forEach((message) => {
+    const className = parseInt(message.sender_id) === parseInt(userId) ? 'client' : 'professional';
+    console.log(className);
+    displayMessage(message, className);
+  });
+}
+
   
+
+  async function sendMessage() {
+    const recipientId = recipientInput.dataset.userId;
+    const messageContent = messageInput.value;
     const isoStringWithoutZ = new Date()
       .toISOString()
       .slice(0, 19)
       .replace('T', ' ');
-  
+
     const messageData = {
-      sender_id: senderId,
+      conversation_id: currentConversationId,
+      sender_id: userId,
       recipient_id: recipientId,
       message_content: messageContent,
       message_sent_at: isoStringWithoutZ,
     };
-  
+
     const sendMessageUrl = 'http://localhost:3000/api/messages';
     const options = {
       method: 'POST',
@@ -109,32 +119,24 @@ document.addEventListener('DOMContentLoaded', function () {
       },
       body: JSON.stringify(messageData),
     };
-  
-    try {
-      const response = await fetchData(sendMessageUrl, options);
-      if (response.ok) {
-        showToast('Viesti lähetetty onnistuneesti.');
-        messageInput.value = '';
-        recipientInput.value = ''; // Tyhjennä vastaanottajan valintakenttä
-        displaySentMessage(messageData);
-        fetchMessagesForConversation(recipientId);  // Tyhjennä viestikenttä lähetyksen jälkeen
-      } else {
-        showToast('Viestin lähetys epäonnistui.');
-      }
-    } catch (error) {
-      console.error('Virhe viestin lähetyksessä:', error);
-      showToast('Virhe viestin lähetyksessä.');
-    }
+
+    fetchData(sendMessageUrl, options);
+    console.log('Message sent successfully');
+    messageInput.value = '';
+    displayMessage(messageData, 'client');
   }
-  function displaySentMessage(messageData) {
-    const messagesContainer = document.getElementById('messages-container');
+
+  function displayMessage(message, userId) {
+    const className = parseInt(message.sender_id) === parseInt(userId) ? 'client' : 'professional';
     const messageDiv = document.createElement('div');
-    messageDiv.classList.add('message');
+    messageDiv.className = `message ${className}`;
+    const messageTime = new Date(message.message_sent_at).toLocaleString('fi-FI', { timeZone: 'Europe/Helsinki' });
     messageDiv.innerHTML = `
-        <div class="message-sender">${messageData.sender_id}</div>
-        <div class="message-content">${messageData.message_content}</div>
-        <div class="message-time">${messageData.message_sent_at}</div>
+      <div class="date">${messageTime}</div>
+      <div class="message-text">${message.message_content}</div>
     `;
     messagesContainer.appendChild(messageDiv);
-}
+  }
   
+  
+});
