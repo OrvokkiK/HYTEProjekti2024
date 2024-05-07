c1070ef826f76b19a2e98b46319048c966961114
ainoruo <aino.ruohola@metropolia.fi>
2024-05-07 09:27:38 +0300

    change urls

--- src/index.js
+++ src/index.js
@@ -32,7 +32,9 @@ app.use(logger);
 app.use(express.json());
 
 app.use(express.static('public'));
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/sivusto', express.static(path.join(__dirname, '../public')));
 
 // user routes
 // Users resource (/api/users)
